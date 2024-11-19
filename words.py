6ee5ebe1225e9b7d69b4c1385d5c396603e34433
Vaslav <2key@rambler.ru>
2024-11-19 12:00:00 +0500

    change google translator

--- words.py
+++ words.py
@@ -1,17 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+from deep_translator import GoogleTranslator
+translator = GoogleTranslator(source='en', target='ru')
 
-def get_word_en():
+def get_word():
     url = 'https://randomword.com/'
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
-        words_en = soup.find('div', id='random_word')
-        words_definition = soup.find('div', id='random_word_definition')
+        word_en = soup.find('div', id='random_word').text.strip()
+        def_en = soup.find('div', id='random_word_definition').text.strip()
+
+        # Перевод
+        word_ru = translator.translate(word_en)
+        def_ru = translator.translate(def_en)
 
         return {
-            'word': words_en.text.strip(),
-            'definition': words_definition.text.strip()
+            'word': word_ru,
+            'definition': def_ru
         }
     except Exception as e:
         print(f"Ошибка: {e}")
@@ -20,7 +26,7 @@ def get_word_en():
 def game():
     print('Начинаем игру')
     while True:
-        words_dict = get_word_en()
+        words_dict = get_word()
         word = words_dict.get('word')
         definition = words_dict.get('definition')
         print(f'Значение слова - {definition}')
